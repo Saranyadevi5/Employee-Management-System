f76ff4e16476877e715f4f1a8a0c18eda416a38b
Saranyadevi5 <115878392+Saranyadevi5@users.noreply.github.com>
2024-02-26 22:06:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
